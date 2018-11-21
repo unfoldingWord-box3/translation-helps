c70c08a3998ae9f4a5f61cf1376762230666249b
Christopher Klapp <christopher@klapp.name>
2018-11-21 12:01:12 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.798dd2a6d57ed9bf5e9339613bb5da35.js"
+  "/precache-manifest.4a8800edec01ef966ff320357b3a2b52.js"
 );
 
 workbox.clientsClaim();
