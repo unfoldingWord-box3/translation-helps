7428a2cdbf7d9b90ec5ba2713ed786bb0994e2f5
Christopher Klapp <christopher@klapp.name>
2018-11-21 12:33:50 -0500

    make cors work with proxy

--- src/ApplicationHelpers.js
+++ src/ApplicationHelpers.js
@@ -77,7 +77,7 @@ export const fetchFileFromServer = (username, repository, filepath, branch='mast
 });
 
 export const get = (_uri) => new Promise((resolve, reject) => {
-  const uri = uriBase + _uri;
+  const uri = 'https://cors-anywhere.herokuapp.com/' + uriBase + _uri;
   fetch(uri, { mode: 'cors' }).then(response => {
     resolve(response.text())
   }).catch(reject);
