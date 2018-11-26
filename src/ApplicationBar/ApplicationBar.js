98b3ddfdb6b16eceef4eb157d25fee3db0c7018b
Christopher Klapp <christopher@klapp.name>
2018-11-26 11:34:44 -0500

    updated theme colors

--- src/ApplicationBar/ApplicationBar.js
+++ src/ApplicationBar/ApplicationBar.js
@@ -51,7 +51,7 @@ ApplicationBar.propTypes = {
 const styles = theme => ({
   paper: {
     color: '#fff',
-    backgroundColor: '#3f51b5',
+    backgroundColor: '#31ADE3',
     textAlign: 'center',
     boxShadow: '0px 1px 5px 0px rgba(0, 0, 0, 0.2),' +
                '0px 2px 2px 0px rgba(0, 0, 0, 0.14),' +
