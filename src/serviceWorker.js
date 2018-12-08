4fa43c63152523ff7eb51a8827d92ede05f71e8b
Christopher Klapp <christopher@klapp.name>
2018-12-08 17:45:02 -0500

    register after unregister for offline.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -74,6 +74,7 @@ function registerValidSW(swUrl, config) {
               //   config.onUpdate(registration);
               // }
               unregister();
+              register();
             } else {
               // At this point, everything has been precached.
               // It's the perfect time to display a
