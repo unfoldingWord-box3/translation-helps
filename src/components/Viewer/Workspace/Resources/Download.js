1030ffad9800971c32cd3111857974b13bb25bdd
Christopher Klapp <christopher@klapp.name>
2019-03-19 11:10:40 -0400

    Download Resources => Complete and Error state shown

--- src/components/Viewer/Workspace/Resources/Download.js
+++ src/components/Viewer/Workspace/Resources/Download.js
@@ -9,6 +9,10 @@ import {
   Typography,
   CircularProgress,
 } from '@material-ui/core';
+import {
+  DoneAll,
+  ErrorOutline,
+} from '@material-ui/icons';
 
 import * as gitApi from '../../gitApi';
 
@@ -20,9 +24,26 @@ const Component = ({
   },
 }) => {
   const [downloading, setDownloading] = useState(false);
+  const [complete, setComplete] = useState(false);
+  const [error, setError] = useState(false);
 
   const subject = 'All Resources for this Language';
   const title = 'Download for Offline Use';
+
+  const downloadingComponent = (
+    <CircularProgress className={classes.progress} color="secondary" disableShrink />
+  );
+  const completeComponent = (
+    <DoneAll />
+  );
+  const errorComponent = (
+    <ErrorOutline />
+  );
+  let component = <div />;
+  if (error) component = errorComponent;
+  if (complete) component = completeComponent;
+  if (downloading) component = downloadingComponent;
+
   return (
     <Card className={classes.card}>
       <CardContent>
@@ -34,17 +55,7 @@ const Component = ({
         </Typography>
       </CardContent>
       <CardActions className={classes.actions}>
-        {
-          downloading ? (
-            <CircularProgress
-              className={classes.progress}
-              color="secondary"
-              disableShrink
-            />
-          ) : (
-            <div />
-          )
-        }
+        {component}
         <Button
           size="small"
           variant="contained"
@@ -57,7 +68,12 @@ const Component = ({
             }})
             .then(response => {
               setDownloading(false);
+              setComplete(true);
               console.log(response);
+            })
+            .catch(error => {
+              setError(error);
+              console.log(error);
             });
           }}
         >
